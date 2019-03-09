2391b852aef73421a4eae355699542b41cd2b959
curegit <37978051+curegit@users.noreply.github.com>
2019-03-10 00:18:47 +0900

    インスペクター拡張の親クラス

---
+++ Scripts/Editor/SpeedcarEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+
+namespace Speedcar.EditorOnly
+{
+	/// <summary>
+	/// インスペクター拡張のための基底クラス
+	/// </summary>
+	public class SpeedcarEditor : Editor
+	{
+		/// <summary>
+		/// ヘッダを表示をする
+		/// </summary>
+		/// <param name="label">表示する文字列</param>
+		protected void Header(string label)
+		{
+			EditorGUILayout.LabelField(label, EditorStyles.boldLabel);
+		}
+
+		/// <summary>
+		/// 空白をあける
+		/// </summary>
+		protected void Space()
+		{
+			EditorGUILayout.Space();
+		}
+	}
+}
